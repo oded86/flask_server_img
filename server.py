9c5985ddb6b05e8dce13c6934128b389d3a21ca1
Sagie <sageika@gmail.com>
2021-10-01 12:09:15 +0300

    with model

--- server.py
+++ server.py
@@ -10,16 +10,17 @@ import PIL
 import PIL.Image as Image
 
 app = Flask(__name__)
-
+MODEL1= tf.keras.models.load_model('testTransferLerning')
+MODEL2=tf.keras.models.load_model('PoopingOrNotLast')
 @app.route("/runClassify", methods=["POST"])
 def process_image():
     class_names=['poop', 'vanila']
     IMAGE_SHAPE = (224, 224)
-    model = tf.keras.models.load_model('testTransferLerning')
+    # model = tf.keras.models.load_model('testTransferLerning')
     file = request.files['image']
     dog1= Image.open(file.stream).resize(IMAGE_SHAPE)
     dog1 = np.array(dog1)/255.0
-    result2 = model.predict(dog1[np.newaxis, ...])
+    result2 = MODEL1.predict(dog1[np.newaxis, ...])
     score2 = tf.nn.softmax(result2[0])
 
     msg = "This image most likely belongs to {} with a {:.2f} percent confidence.".format(class_names[np.argmax(score2)], 100 * np.max(score2))
@@ -32,11 +33,11 @@ def process_image():
 def process_image_pooping():
     class_names=['pooping', 'not pooping']
     IMAGE_SHAPE = (224, 224)
-    model = tf.keras.models.load_model('PoopingOrNotLast')
+    # model = tf.keras.models.load_model('PoopingOrNotLast')
     file = request.files['image']
     dog1= Image.open(file.stream).resize(IMAGE_SHAPE)
     dog1 = np.array(dog1)/255.0
-    result2 = model.predict(dog1[np.newaxis, ...])
+    result2 = MODEL2.predict(dog1[np.newaxis, ...])
     score2 = tf.nn.softmax(result2[0])
     msg = "This image most likely belongs to {} with a {:.2f} percent confidence.".format(class_names[np.argmax(score2)], 100 * np.max(score2))
     print(msg)
